9a3f682fa1476417c4e9759e738aa36744bb71fd
Malikd96 <96malikdavis@gmail.com>
2024-09-01 12:28:51 -0400

    PyLists

---
+++ Py_Lists_Transformation2.py
@@ -0,0 +1,8 @@
+#Task 2: Calculate the average grade and print it.
+#
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+total = sum(grades)
+num_grades = len(grades)
+average = total / num_grades
+
+print("Grade Average:", average)
